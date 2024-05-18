02a2f90d423c6b4cdb0b25035c744fbcd1159652
Matthew Pereira <matthewdavidpereira@gmail.com>
2024-05-17 21:36:36 -0700

    draft simple stream contract

--- packages/foundry/test/Challenge.t.sol
+++ packages/foundry/test/Challenge.t.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../contracts/Challenge.sol";
+import {Challenge} from "../contracts/Challenge.sol";
 
 contract ChallengeTest is Test {
-    // Put tests here
+    Challenge simpleStream;
 }
